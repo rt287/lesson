d91433e3693ce8af282182c147a6adc37e37dcd6
Rafael Tomas <S2403367@ndonline.ac.uk>
2024-09-23 15:55:10 +0100

    new

--- lesson/lesson.py
+++ lesson/lesson.py
@@ -1,1 +1,10 @@
-﻿
+﻿username = "rafaeltom287"
+password = "Password1"
+
+UserName = input("what is your username")
+Password = input ("what is your password")
+if UserName != username  aPassword != password:
+    print("username or  password is incorrect")
+else:
+    Password == password
+    print("you have logged in successfully")
